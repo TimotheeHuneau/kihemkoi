0855c9871f84a1f1298f35bd19a675aba7480beb
Timothée Huneau <contact@timotheehuneau.fr>
2023-04-05 16:23:32 +0200

    MàJ mineur de CSS et scripts

--- scripts/sAccueil.js
+++ scripts/sAccueil.js
@@ -2,9 +2,7 @@ window.onload = function(){
 	var aAllInput = document.querySelectorAll('.inputChoixNiveau');
 	for (input of aAllInput){
 		input.onchange = function(){
-			this.parentElement.parentElement.className = this.classList[0] + ' case modified';
-			console.log(this.classLis)
+			this.parentElement.className = this.classList[0];
 		}
-		
 	}
 }
