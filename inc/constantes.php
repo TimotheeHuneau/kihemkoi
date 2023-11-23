986d21494af9d403219701048acd0b8185858693
GLenPL <40721911+GLenPL@users.noreply.github.com>
2023-11-23 17:00:00 +0100

    Re-added inc/constantes.php
    (sans les données confidentielles)

---
+++ inc/constantes.php
@@ -0,0 +1,8 @@
+<?php
+	define('SEL', 'larme d\'homophobe');
+	define('HASHED_PASSWORD','lanZLtT0D5CAI');
+	
+	define('D', '@'); //delimiteur, lorsque plusieurs info sont passées en POST, sous forme de chaîne de caractère
+	
+	define('SEPARATEUR', '&#x00B7');
+?>
